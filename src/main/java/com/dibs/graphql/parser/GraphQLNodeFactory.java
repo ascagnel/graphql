c3928edfda54c6a84924d8831db4582aeba4a084
Matthew Mason <mjm@matthewjmason.com>
2015-06-23 07:38:30 -0400

    Refactored parsing logic

--- src/main/java/com/dibs/graphql/parser/GraphQLNodeFactory.java
+++ src/main/java/com/dibs/graphql/parser/GraphQLNodeFactory.java
@@ -1,14 +1,16 @@
 package com.dibs.graphql.parser;
 
-import com.dibs.graphql.parser.data.GraphQLNode;
-import com.dibs.graphql.parser.data.GraphQLToken;
+import com.dibs.graphql.data.Query;
+import com.dibs.graphql.data.QueryBuilder;
+import com.dibs.graphql.data.parse.QueryToken;
 
 public class GraphQLNodeFactory {
 
-	public static GraphQLNode fromToken(GraphQLToken token) {
-		GraphQLNode node = new GraphQLNode()
-			.value(token.getValue())
-			.attributes(token.getAttributes());
+	public static Query fromToken(QueryToken token) {
+		Query node = new QueryBuilder()
+			.name(token.getValue())
+			.params(token.getAttributes())
+			.build();
 		
 		return node;
 	}
